f91d942e07425ea78d2ced3e962c332618a7b6a7
Marcelo2500 <marcelo.lizana25@gmail.com>
2022-04-23 23:57:12 -0400

    Revert "Merge branch 'main' of https://github.com/Moon3301/ProyectoLibreria"
    This reverts commit 3ff6ce6b7bd79cc54dac388d6a71d0305a34cbd7, reversing
    changes made to 617bdab95c3498adf509d58a164a8deeed99d871.

--- entorno001/libreria/app_libreria/models.py
+++ entorno001/libreria/app_libreria/models.py
@@ -17,13 +17,12 @@ class Libro(models.Model):
     id       = models.IntegerField(primary_key=True)
     nombre   = models.CharField(max_length=50)
     autor    = models.CharField(max_length=50)
-    imagen   = models.CharField(max_length=50)
+    imagen   = models.ImageField()
     editorial= models.CharField(max_length=50)
-    stock    = models.IntegerField()
     def __str__(self):
         return self.nombre
     
-class Tienda(models.Model):
+class Libro(models.Model):
     id        = models.IntegerField(primary_key=True)
     ubicacion = models.CharField(max_length=50)
     def __str__(self):
